86950a90647541a07f2a1abb05ec23c526e1278d
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-06-09 15:05:48 -0300

    feature: aAPI - GestÃão Biblioteca

---
+++ src/main/java/com/lucasvargas/dev/api_gerenciamento_biblioteca/ApiGerenciamentoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.lucasvargas.dev.api_gerenciamento_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentoBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciamentoBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/lucasvargas/dev/api_gerenciamento_biblioteca/model/AutorModel.java
@@ -0,0 +1,4 @@
+package com.lucasvargas.dev.api_gerenciamento_biblioteca.model;
+
+public class AutorModel {
+}
