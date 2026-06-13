9a983b3b9e5612234e7c5e647fc076c8513b46a7
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-06-13 11:42:54 -0300

    feature: add LivroModel by entitys

--- src/main/java/com/lucasvargas/dev/api_gerenciamento_biblioteca/model/LivroModel.java
+++ src/main/java/com/lucasvargas/dev/api_gerenciamento_biblioteca/model/LivroModel.java
@@ -1,4 +1,27 @@
 package com.lucasvargas.dev.api_gerenciamento_biblioteca.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_livro")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class LivroModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private AutorModel autorModel;
+
 }
