782eb7964e3a328eb33cecc4c8b9bc5f7efd3b63
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-06-13 11:59:25 -0300

    feature: add AutorModel by entity

--- src/main/java/com/lucasvargas/dev/api_gerenciamento_biblioteca/model/AutorModel.java
+++ src/main/java/com/lucasvargas/dev/api_gerenciamento_biblioteca/model/AutorModel.java
@@ -1,4 +1,27 @@
 package com.lucasvargas.dev.api_gerenciamento_biblioteca.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_autor")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class AutorModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String sobre;
+    private LocalDate dataDeNascimento;
+    private String nacionalidade;
 }
