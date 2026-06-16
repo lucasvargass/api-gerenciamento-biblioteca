3a82143eda0809a91fbe0c4f95f97484fa2400f2
Lucas Vargas <lucas.vargas.pina25@gmail.com>
2026-06-16 09:33:35 -0300

    feature: add a class AutorController com rotas definidas

---
+++ src/main/java/com/lucasvargas/dev/api_gerenciamento_biblioteca/controller/AutorController.java
@@ -0,0 +1,48 @@
+package com.lucasvargas.dev.api_gerenciamento_biblioteca.controller;
+
+import com.lucasvargas.dev.api_gerenciamento_biblioteca.model.AutorModel;
+import com.lucasvargas.dev.api_gerenciamento_biblioteca.service.AutorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/autor")
+@RequiredArgsConstructor
+public class AutorController {
+
+    private final AutorService autorService;
+
+    @PostMapping
+    public ResponseEntity<String> cadastrarAutor(@PathVariable AutorModel autorCadastrado){
+        autorService.cadastrarAutor(autorCadastrado);
+        return ResponseEntity.ok("Autor criado com sucesso!");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AutorModel>> buscarAutor(){
+        return ResponseEntity.ok(autorService.listarTodosAutores());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AutorModel> buscarAutorById(@PathVariable Long id){
+        return ResponseEntity.ok(autorService.buscarAutorPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizarAutor(@PathVariable Long id, @RequestBody AutorModel autorAtualizar){
+        autorService.atualizarAutor(id, autorAtualizar);
+        return ResponseEntity.ok("Autor editado com sucesso!");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarAutor(@PathVariable Long id){
+        autorService.deletarAutor(id);
+        return ResponseEntity.ok("Autor deletado com sucess!");
+    }
+
+}
